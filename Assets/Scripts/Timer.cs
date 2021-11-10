df264e28f2f0d10ecd607b0a9aaa307b49f2e48d
Mika Cuvillier <mika.cuvillier@gmail.com>
2021-11-10 10:02:45 -0500

    Chronomètre

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
+    public float valeurTemps = 900;
+    public Text chronometre;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,27 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(valeurTemps > 0)
+        {
+            valeurTemps -= Time.deltaTime;
+        }
+        else
+        {
+            valeurTemps = 0;
+        }
+        AfficherTemps(valeurTemps);
+    }
+
+    void AfficherTemps(float tempsAffiche)
+    {
+        if(tempsAffiche < 0)
+        {
+            tempsAffiche = 0;
+        }
+
+        float minutes = Mathf.FloorToInt(tempsAffiche / 60);
+        float secondes = Mathf.FloorToInt(tempsAffiche % 60);
+
+        chronometre.text = string.Format("{0:00}:{1:00}", minutes, secondes);
     }
 }
